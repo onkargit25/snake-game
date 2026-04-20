b2e963be23d87d8ff29ea6172e82ae77218f6052
unknown <onkarpawar2007@gmail.com>
2026-04-20 19:33:49 +0530

    Changes as told before in previosu commit

--- SnakeMain.java
+++ SnakeMain.java
@@ -1,3 +1,17 @@
+import javax.swing.JFrame;
+
+class SnakeOnScreen{
+
+    void setSnakeOnScreen(Snake[] snake, int[][] screen, int snakeLength){
+        for(int i = 0; i < snakeLength ; i++){
+            screen[(snake[i]).y][snake[i].x] = 1;
+        }
+    }
+    
+
+    SnakeOnScreen(){
+    }
+}
 class Snake{
     int x;
     int y;
@@ -9,6 +23,28 @@ class Snake{
 }
 
 class SnakeHead extends Snake{
+
+    //North, South, East, West
+    void move(char direction){
+
+        switch(direction){
+            case 'N':
+                this.y++;
+                break;
+
+            case 'E':
+                this.x++;
+                break;
+
+            case 'S':
+                this.y--;
+                break;
+
+            case 'W':
+                this.x--;
+                break;
+        }
+    }
     
     SnakeHead(int x, int y){
         super(x, y);
@@ -28,9 +64,46 @@ class Food{
 class SnakeMain{
     public static void main(String args[]){
 
+
         Snake[] snake = new Snake[400];
         snake[0] = new SnakeHead(10, 10);
         snake[1] = new Snake(9, 10);
+        snake[2] = new Snake(8, 10);
+
+        int snakeLength = 3;
+
+
+        int[][] screen = new int[20][20];
+
+        final int SNAKE_BODY = 1;
+        final int SNAKE_HEAD = 2;
+        final int GRASS = 0;
+
+        SnakeOnScreen ss = new SnakeOnScreen();
+        ss.setSnakeOnScreen(snake, screen, snakeLength);
+
+        for(int i = 0; i < 20; i++){
+            for(int j = 0; j < 20; j++){
+                System.out.print(screen[i][j]);
+            }
+
+            System.out.println();
+        }
+        
+
+        JFrame frame = new JFrame("Snake game");
+        SnakePanel panel = new SnakePanel(screen);
+
+        
+
+        frame.add(panel);
+        frame.setSize(panel.screenWidth, panel.screenHeight);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null); // Centers the window
+        frame.setVisible(true);
+
+
+        
 
     }//end of main
 }//end of class
